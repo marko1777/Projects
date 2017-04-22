d13ac12309520b652efd52b99551b547f350bea9
Mark Puha <p.mark95@gmail.com>
2017-04-22 21:54:05 +0200

    Add files via upload

--- algo2/main.cpp
+++ algo2/main.cpp
@@ -3,161 +3,196 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <stdlib.h>
+#include <time.h>
+#include "queue.h"
 
 enum Color{
   WHITE,
   BLACK,
   GRAY
 };
-//class GraphDiameter;
 
 class GraphVertex
 {
 public:
-  //friend GraphDiameter;
-  // trick: http://en.cppreference.com/w/cpp/language/class
-  // just above "Member specification"
-  //
-  // Note: Im not sure whats going on, but im pretty convinced that
-  //   it is related someway to name lookup and thats why it ccould not find the typedef
-  //   and yea, "friend class" forward decls a type as you know
-  //   You can check this, making a pointer to the forward declared type
-  //   and the compiler will happily allow it for you, becouse it doesnt have to know the
-  //   whole definition for a pointer, and recognizes the name as a type.
-  //   As a last note, i repeate that i dont really know whats going on, but happens on gcc and clang too
   friend class GraphDiameter;
   GraphVertex(Color vC = WHITE,int d = 0) : vertexColor(vC), distance(d){}
-  friend std::istream& operator>>(std::istream &in, class GraphDiameter &g);
-  friend std::ostream& operator<<(std::ostream &out,const class GraphDiameter &g);
+  friend std::istream& operator>>(std::istream &in, class GraphDiameter &gD);
+  friend std::ostream& operator<<(std::ostream &out, const class GraphDiameter &gD);
+  friend std::ostream& operator<<(std::ostream &out, const GraphVertex &gV);
 private:
   Color vertexColor;
   int distance;
-  std::vector<int> adjAr;
+  std::vector<int> vertexAdjacentVec;
 };
+
+enum Exceptions{NOT_POSITIVE_COUNT};
+
+void throwIfNotPositiveCount(int throwValue)
+{
+  if(throwValue <= 0) throw NOT_POSITIVE_COUNT;
+}
 
 class GraphDiameter{
 public:
+  void initStartVertexAndQueue();
   int calculateDiameter();
-  void calculateDistance(int examinedAdj, int &vertexDepth);
-  void pushToMaxDistanceVec(int pushingElement);
-  void calculateDistanceAndPushToMaxDistanceVec(int firstVertex);
-  int firstTwoElementSumFromMaxDistanceVec();
-  void recursiveCallForCalculateDistanceAndDecideMaxDistance(int actAdj,int &vertexDepth);
-  friend std::istream& operator>>(std::istream &in, GraphDiameter &g);
-  friend std::ostream& operator<<(std::ostream &out,const GraphDiameter &g);
+  void calculateDistanceWithBfs();
+  void reInitVertexesAndQueue();
+  void incrementAdjDistanceAndPushToQueue(int actVertex, int actAdjacent);
+  void decideMaxDistanceInd(int actVertex, int actAdjacent);
+  int checkStartVertex(int startVertex);
+  friend std::istream& operator>>(std::istream &in, GraphDiameter &gD);
+  friend std::ostream& operator<<(std::ostream &out, const GraphDiameter &gD);
+  friend std::ostream& operator<<(std::ostream &out, const GraphVertex &gV);
 private:
-  int maxDistance;
+  int maxDistanceInd;
+  MyQueue::Queue graphQueue;
   std::vector<GraphVertex> graphVertexVec;
-  std::vector<int> maxDistanceVec;
 };
 
-std::istream& operator>>(std::istream &in, GraphDiameter &g)
+std::istream& operator>>(std::istream &in, GraphDiameter &gD)
 {
     int vertexDb;
     in >> vertexDb;
-    g.graphVertexVec.resize(vertexDb + 1);
+    throwIfNotPositiveCount(vertexDb);
+    gD.graphVertexVec.resize(vertexDb + 1);
     int u, v;
     in >> u;
-    while(u)
+    while(u != 0)
     {
       in >> v;
-      g.graphVertexVec[u].adjAr.push_back(v);
-      g.graphVertexVec[v].adjAr.push_back(u);
+      gD.graphVertexVec[u].vertexAdjacentVec.push_back(v);
+      gD.graphVertexVec[v].vertexAdjacentVec.push_back(u);
       in >> u;
     }
     in >> u; //Az utolsó nulla beolvasása miatt
   return in;
 }
-std::ostream& operator<<(std::ostream &out,const GraphDiameter &g)
+
+std::ostream& operator<<(std::ostream &out,const GraphDiameter &gD)
 {
-  for(auto i = 1u; i < g.graphVertexVec.size(); i++)
+  for(auto i = 1u; i < gD.graphVertexVec.size(); i++)
   {
-    out << i << ".vertex adjacments: ";
-    for(auto j = 0u; j < g.graphVertexVec[i].adjAr.size(); j++)
+    out << i << ".vertex adjacent: ";
+    for(auto j = 0u; j < gD.graphVertexVec[i].vertexAdjacentVec.size(); j++)
     {
-      out << g.graphVertexVec[i].adjAr[j] << " "; 
+      out << gD.graphVertexVec[i].vertexAdjacentVec[j] << " "; 
     }
     out << std::endl;
   }
   return out;
 }
+
+std::ostream& operator<<(std::ostream &out, const GraphDiameter::GraphVertex &gV)
+{
+  for(auto i = 0u; i < gV.vertexAdjacentVec.size(); ++i)
+  {
+    out << i + 1 << " .Adjacent: " << gV.vertexAdjacentVec[i] << " ";
+  }
+  out << std::endl;
+
+  return out;
+}
+
 int GraphDiameter::calculateDiameter()
 {
-  int firstVertex = 1;
-  calculateDistanceAndPushToMaxDistanceVec(firstVertex);
-  if(maxDistanceVec.size() >= 2u)
+    initStartVertexAndQueue();
+    calculateDistanceWithBfs();
+    reInitVertexesAndQueue();
+    calculateDistanceWithBfs();
+    return graphVertexVec[maxDistanceInd].distance;
+}
+
+void GraphDiameter::initStartVertexAndQueue()
+{
+    srand(time(NULL));
+    maxDistanceInd = 1;
+    int startVertex = (rand() % (graphVertexVec.size()));
+    graphVertexVec[startVertex].vertexColor = GRAY;
+    graphQueue.push(checkStartVertex(startVertex));
+}
+
+int GraphDiameter::checkStartVertex(int startVertex)
+{
+  return startVertex == 0 ? 1 : startVertex;
+}
+
+void GraphDiameter::calculateDistanceWithBfs()
+{
+  while(!graphQueue.isEmpty())
   {
-    std::sort(maxDistanceVec.begin(),maxDistanceVec.end(), std::greater<int>());
-    return maxDistance = firstTwoElementSumFromMaxDistanceVec();
-  }else{
-    return maxDistance = maxDistanceVec.front();
+     const auto actVertex = graphQueue.pop();
+     for(auto i = 0u; i < graphVertexVec[actVertex].vertexAdjacentVec.size(); i++)
+      {
+        const auto actAdjacent = graphVertexVec[actVertex].vertexAdjacentVec[i];
+        if(graphVertexVec[actAdjacent].vertexColor == WHITE)
+        {
+          incrementAdjDistanceAndPushToQueue(actVertex, actAdjacent);
+          decideMaxDistanceInd(actVertex, actAdjacent);
+        }
+      }
+      graphVertexVec[actVertex].vertexColor = BLACK;
   }
 }
-void GraphDiameter::calculateDistanceAndPushToMaxDistanceVec(int firstVertex)
+
+void GraphDiameter::incrementAdjDistanceAndPushToQueue(int actVertex, int actAdjacent)
 {
-  maxDistance = 0;
-  graphVertexVec[firstVertex].vertexColor = GRAY;
-  for(auto i = 0u; i < graphVertexVec[firstVertex].adjAr.size(); i++)
-  {
-    int mainDepth = 0;
-    int actAdj = graphVertexVec[firstVertex].adjAr[i];
-    calculateDistance(actAdj, mainDepth);
-    pushToMaxDistanceVec(mainDepth);
-    graphVertexVec[actAdj].vertexColor = BLACK;
-  }
+    graphVertexVec[actAdjacent].distance = graphVertexVec[actVertex].distance + 1;
+    graphVertexVec[actAdjacent].vertexColor = GRAY;
+    graphQueue.push(actAdjacent);
 }
-void GraphDiameter::calculateDistance(int examinedAdj, int &vertexDepth)
+
+void GraphDiameter::decideMaxDistanceInd(int actVertex, int actAdjacent)
 {
-  graphVertexVec[examinedAdj].distance = ++vertexDepth;
-  graphVertexVec[examinedAdj].vertexColor = GRAY;
-  for(auto i = 0u; i < graphVertexVec[examinedAdj].adjAr.size(); i++)
-  {
-    int actAdj = graphVertexVec[examinedAdj].adjAr[i];
-    if(graphVertexVec[actAdj].vertexColor == WHITE)
+    if(graphVertexVec[maxDistanceInd].distance <  graphVertexVec[actAdjacent].distance)
     {
-      recursiveCallForCalculateDistanceAndDecideMaxDistance(actAdj,vertexDepth);
-    }
-  }
-  graphVertexVec[examinedAdj].vertexColor = BLACK;
+        maxDistanceInd = actAdjacent;
+    }   
 }
-void GraphDiameter::recursiveCallForCalculateDistanceAndDecideMaxDistance(int actAdj,int &vertexDepth)
+
+void GraphDiameter::reInitVertexesAndQueue()
 {
-    int postVertexDepth = vertexDepth;
-    calculateDistance(actAdj,postVertexDepth);
-    graphVertexVec[actAdj].vertexColor = BLACK;
-    if(maxDistance < postVertexDepth)
+    graphQueue.push(maxDistanceInd);
+    for(auto i = 1u; i < graphVertexVec.size();++i) 
     {
-      maxDistance = postVertexDepth;
-    }
+        graphVertexVec[i].vertexColor = WHITE;
+        graphVertexVec[i].distance = 0;
+    }  
 }
-void GraphDiameter::pushToMaxDistanceVec(int pushingElement)
+
+void mainProcedure()
 {
-    if(maxDistance != 0)
+    std::string fileName;
+    std::cout << "Type your input file:"; std::cin >> fileName;
+    std::ifstream fileIn(fileName);
+    std::ofstream fileOut("out.txt");
+    int numberOfGraphs;
+    fileIn >> numberOfGraphs;
+    throwIfNotPositiveCount(numberOfGraphs);
+    for(int i = 0; i < numberOfGraphs; i++)
     {
-      maxDistanceVec.push_back(maxDistance);
-      maxDistance = 0;
-    }else{
-      maxDistanceVec.push_back(pushingElement);
+      GraphDiameter gD;
+      fileIn >> gD;
+      int actGraphDiameter = gD.calculateDiameter();
+      std::cout << i + 1 << " .Graph Diameter is: "<<actGraphDiameter << std::endl;
+      std::cout << gD.graphVertexVec[actGraphDiameter] << std::endl;
+      fileOut << i + 1 << " .Graph Diameter is: "<<actGraphDiameter << std::endl;
+      fileOut << gD.graphVertexVec[actGraphDiameter] << std::endl;
     }
 }
-int GraphDiameter::firstTwoElementSumFromMaxDistanceVec()
-{
-  int a = maxDistanceVec.front();
-  maxDistanceVec.pop_back();
-  int b = maxDistanceVec.front();
-  maxDistanceVec.pop_back();
-  return a + b;
-}
+
 int main()
 {
-  std::ifstream f("input.txt");
-  int numberOfGraphs;
-  f >> numberOfGraphs;
-  for(int i = 0; i < numberOfGraphs; i++)
+  try{
+    mainProcedure();
+  }catch(Exceptions e)
   {
-    GraphDiameter gD;
-    f >> gD;
-    //std::cout << dg; Szomszédok kiírása
-    std::cout << gD.calculateDiameter();
+    if (e == NOT_POSITIVE_COUNT) std::cout << "The input must be positive" << std::endl;    
+  }
+  catch(MyQueue::Queue::Exceptions e)
+  {
+    if(e == MyQueue::Queue::EMPTY_LIST) std::cout << "You can't erase if your List is empty" << std::endl;
   }
 }
