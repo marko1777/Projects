31428672f387c340ca62412eeaea805cd0b7ea5c
Mark Puha <p.mark95@gmail.com>
2017-04-23 15:52:40 +0200

    Add files via upload

--- script/bead02.py
+++ script/bead02.py
@@ -3,13 +3,31 @@ import re
 import fileinput
 import glob
 
-sequence = "+(\s;).+|.+"
-anything = ".+?\s(\[\[).+?\s(\".+\"|\w+)((\s*;)|(\]\])).+?\s(\".+\"|\w+)((\s*;)|(\]\]))"
-loop = "(C)IKLUS" + anything
-branching = "(E)LAGAZAS" + anything
+sequence = ".*"
+manual = "(\[\[).*(\]\])"
+loop = "(CIKLUS)" + sequence + manual
+branching = "(ELAGAZAS)" + sequence + manual
 
 for file in glob.glob("*.prog"):
     inF = open(file,"r")
+    outF = open(file.split(".")[0] + ".py","w")
     for itInF in inF:
         inFTemp = itInF.strip()
-        inFMap = re.match("loop|branching|sequence",inFTemp)
+        matchValue = loop + "|" + branching + "|" + sequence 
+        inFMap = re.match(matchValue,inFTemp)
+        tempString = str(inFMap.group())
+        if str(inFMap.group(1)) == "CIKLUS":
+            tempString = tempString.replace("CIKLUS","for")
+        if str(inFMap.group(4)) == "ELAGAZAS":
+            tempString = tempString.replace("ELAGAZAS","if")
+        tempString = tempString.replace("[[",":\n    ")
+        while tempString.find(";") != -1: 
+            tempString = tempString.replace(";","\n    ")
+        prevTempString = tempString
+        tempString = tempString.replace("]]","\n")
+        if prevTempString == tempString:
+            outF.write(tempString + "\n")            
+        else:
+            outF.write(tempString)
+    inF.close()
+    outF.close()
